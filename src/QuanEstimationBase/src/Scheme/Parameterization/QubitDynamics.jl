7a9af3dea22379c9e24ee4e97acc5a5a07fd6acd
Jing Liu <liujing@hainanu.edu.cn>
2025-01-18 21:06:18 +0800

    modify QubitDephasing

--- src/QuanEstimationBase/src/Scheme/Parameterization/QubitDynamics.jl
+++ src/QuanEstimationBase/src/Scheme/Parameterization/QubitDynamics.jl
@@ -2,7 +2,10 @@ function QubitDephasing(r::Vector{Float64},
                         para_est::String,
                         gamma::Float64,  
                         tspan::Union{Vector{Float64}, StepRangeLen})
+    # Hamiltonian
     H0 = r[1]*SigmaX()+r[2]*SigmaY()+r[3]*SigmaZ()
+
+    # Choose the derivative of the Hamiltonian according to the parameter to be estimated
     if para_est == "x"
         dH = [SigmaX()]
     elseif para_est == "y"
@@ -10,6 +13,8 @@ function QubitDephasing(r::Vector{Float64},
     elseif para_est == "z"
         dH = [SigmaZ()]
     end
+
+    # Define the decay
     decay = [[SigmaZ()], [gamma]]
     return Lindblad(H0, dH, tspan, decay, dyn_method=:Ode)
-end
+end # function QubitDephasing
