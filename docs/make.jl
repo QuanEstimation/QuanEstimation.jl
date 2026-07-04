274ba253af164455c20186966f09d7ee584e8f34
Azw <74289473+azwphy@users.noreply.github.com>
2026-07-04 11:14:09 +0800

    Modify deploydocs and enhance Users Guide structure
    Updated the deploydocs function to specify the devbranch as 'main' and added additional pages to the Users Guide.

--- docs/make.jl
+++ docs/make.jl
@@ -16,9 +16,19 @@ makedocs(
     authors = "QuanEstimation Group (Jing Liu et al.)",
     repo = "https://github.com/QuanEstimation/QuanEstimation.jl/blob/{commit}{path}#{line}",
     sitename = "QuanEstimation.jl",
-    edit_link = "GitHub",
     pages = [
         "Home" => "index.md",
+        "Users Guide" => [
+            "Parameterization" => "guide/guide_dynamics.md",
+            "Metrological Tools" => "guide/guide_bounds.md",
+            "Control Optimization" => "guide/guide_Copt.md",
+            "State Optimization" => "guide/guide_Sopt.md",
+            "Measurement Optimization" => "guide/guide_Mopt.md",
+            "Comprehensive Optimization" => "guide/guide_Compopt.md",
+            "Adaptive Schemes" => "guide/guide_adaptive.md",
+            "Resources" => "guide/guide_resources.md",
+            "Output Files" => "guide/output_files.md",
+        ],
         "API Reference" => [
             "General API" => "api/GeneralAPI.md",
             "Base API" => "api/BaseAPI.md",
@@ -27,4 +37,4 @@ makedocs(
     ],
 )
 
-deploydocs(; repo = "github.com/QuanEstimation/QuanEstimation.jl")
+deploydocs(; repo = "github.com/QuanEstimation/QuanEstimation.jl", devbranch = "main")
